3d5f8152e07f4eda6ae64efe93a8b33d4ac303bb
Matt Keys <MatthewjKeys22@gmail.com>
2025-05-20 12:48:37 -0400

    search component added

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CollectionComponent } from './collection/collection.component';
 import { BagComponent } from './bag/bag.component';
+import { DiscSearchComponent } from './disc-search/disc-search.component';
 
 export const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'collection', component: CollectionComponent },
   { path: 'bag', component: BagComponent },
-  //   { path: 'search', component:SearchComponent },
+  { path: 'search', component: DiscSearchComponent },
 ];
---
+++ ui/src/app/disc-search/disc-search.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-disc-search',
+  imports: [],
+  templateUrl: './disc-search.component.html',
+  styleUrl: './disc-search.component.scss'
+})
+export class DiscSearchComponent {
+
+}
