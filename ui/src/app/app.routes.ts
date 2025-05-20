24b0f57bdb93ed7a8380181a0eb2ca8943dc2659
Matt Keys <MatthewjKeys22@gmail.com>
2025-05-20 15:37:09 -0400

    login addd

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { CollectionComponent } from './collection/collection.component';
 import { BagComponent } from './bag/bag.component';
 import { DiscSearchComponent } from './disc-search/disc-search.component';
 import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'collection', component: CollectionComponent },
   { path: 'profile', component: ProfileComponent },
+  { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: '' },
   // { path: 'search', component: DiscSearchComponent },
 ];
