37984eb92b91bc2dfca4ce97c6f48be19eacc507
Matt Keys <MatthewjKeys22@gmail.com>
2025-05-20 13:27:00 -0400

    alright im gunna do tialwinds

--- ui/src/app/collection/collection.component.ts
+++ ui/src/app/collection/collection.component.ts
@@ -1,11 +1,45 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../shared/modules/material.module';
+import { DiscService } from '../shared/services/disc.service';
+import { MatDialog } from '@angular/material/dialog';
+import { DiscSearchComponent } from '../disc-search/disc-search.component';
+import { Disc } from '../shared/interfaces/disc.interface';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-collection',
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './collection.component.html',
-  styleUrl: './collection.component.scss'
+  styleUrl: './collection.component.scss',
 })
 export class CollectionComponent {
+  collection;
 
+  constructor(private discService: DiscService, private dialog: MatDialog) {
+    this.collection = this.discService.collection;
+  }
+
+  ngOnInit(): void {
+    this.discService.setCollection(1);
+  }
+
+  openDiscSearchModal() {
+    const dialogRef = this.dialog.open(DiscSearchComponent);
+    dialogRef
+      .afterClosed()
+      .pipe(take(1))
+      .subscribe((result) => {});
+  }
+
+  addDiscToBag(disc: Disc): void {
+    const updatedDisc = { ...disc, bag: true };
+    const newDisc = this.discService
+      .updateDisc(updatedDisc)
+      .pipe(take(1))
+      .subscribe();
+  }
+
+  deleteDisc(discId: number) {
+    this.discService.deleteDisc(discId).pipe(take(1)).subscribe();
+  }
 }
