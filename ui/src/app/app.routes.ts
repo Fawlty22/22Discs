c22ef328c9469b2d463457be88b93e1d76c5fd98
Matt Keys <MatthewjKeys22@gmail.com>
2025-05-20 12:46:37 -0400

    routes

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CollectionComponent } from './collection/collection.component';
+import { BagComponent } from './bag/bag.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'collection', component: CollectionComponent },
+  { path: 'bag', component: BagComponent },
+  //   { path: 'search', component:SearchComponent },
+];
--- ui/src/app/shared/modules/material.module.ts
+++ ui/src/app/shared/modules/material.module.ts
@@ -14,6 +14,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   exports: [
@@ -30,6 +31,7 @@ import { MatMenuModule } from '@angular/material/menu';
     MatSnackBarModule,
     MatProgressSpinnerModule,
     MatMenuModule,
+    MatTooltipModule,
   ],
 })
 export class MaterialModule {}
